7c4a6b0b87a40ba1b560931b6e4a27c38e60af5a
Anoduck, The Anonymous Duck <11767-anoduck@users.noreply.gitgud.io>
2024-06-16 14:49:24 -0400

    added cfg files to gitignore and motified install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 # Update and Install Dependencies
 apt update -y
-apt install -y python3 python3-dev virtualenv libpcap-dev
+apt install -y python3 python3-dev virtualenv libpcap-dev python3-dnspython python3-pcapy
+
+## ---------------------------------------------------------------------------
+# Rather than use virtualenv to install the remaining two requirements,
+# just install them with the distrobution package management tools as you would
+# any other package. Using virtualenv to do this for you is rather pointless since
+# the versions of these additional libraries are not pinned to any particular
+# version, and the use of virtualenv was solely to satisfy the "best practices"
+# method.
+## ---------------------------------------------------------------------------
 
 # Create virtualenv for script
-virtualenv -p python3 venv
-source venv/bin/activate
+# virtualenv -p python3 venv
+# source venv/bin/activate
 
 # Install PyPi dependencies
-pip install dnspython pcapy
+# pip install dnspython pcapy
